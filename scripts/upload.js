77d8b203bd537ddd911d45fb82b9ed115c589a55
Jr Gouveia <gouvik.dev@gmail.com.br>
2023-06-13 00:50:44 -0300

    feat: Implementando progress bar

---
+++ scripts/upload.js
@@ -0,0 +1,17 @@
+var input = document.querySelector("#image");
+
+document.querySelector("button").addEventListener("click", function () {
+    input.click();
+});
+
+input.addEventListener("change", preview);
+function preview () {
+    var fileObject = this.files[0];
+    var fileReader = new FileReader();
+    fileReader.readAsDataURL(fileObject);
+    fileReader.onload = function () {
+        var result = fileReader.result;
+        var img = document.querySelector("#preview");
+        img.setAttribute("src", result);
+    };
+}
